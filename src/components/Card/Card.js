1089cef9e5b44271796b90224e0b5a732a57650a
Luke Finney <lucaswfinney@gmail.com>
2017-11-02 13:28:58 -0600

    adding toggle functionality for card click

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ house, showActiveMembers, toggleActiveMembers }) => {
+class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showMembers: false
+    };
+  }
 
-  const seats = house.seats.map( seat =>
-    <p key={seat} >{seat}</p>);
-  const titles = house.titles.map( title =>
-    <p key={title} >{title}</p>);
-  const weapons = house.ancestralWeapons.map( weapon =>
-    <p key={weapon} >{weapon}</p>);
-  const members = house.swornMembers.map( (member, index) =>
-    <p key={index}>{member}</p> )
+  toggleActiveMembers() {
+    console.log('whodis');
+    const toggle = this.state.showMembers ? true : false;
+    this.setState({
+      showMembers: toggle
+    });
+  }
 
-  return (
-    <div
-      className="card"
-      onClick={() => toggleActiveMembers(true)}>
-      <h2>{house.name}</h2>
-      <p>{house.founded}</p>
-      <h3>House Seats</h3>
-      <h3>{seats}</h3>
-      <h3>House Titles</h3>
-      <div>{titles}</div>
-      <h3>Coat of Arms</h3>
-      <p>{house.coatOfArms}</p>
-      <h3>Ancestral Weapons</h3>
-      <div>{weapons}</div>
-      <h3>{house.words}</h3>
-      {showActiveMembers &&
-      <div>{members}</div>
-      }
-    </div>
-  );
-};
+  render() {
+    const { house } = this.props;
+    const seats = this.props.house.seats.map( seat =>
+      <p key={seat} >{seat}</p>);
+    const titles = house.titles.map( title =>
+      <p key={title} >{title}</p>);
+    const weapons = house.ancestralWeapons.map( weapon =>
+      <p key={weapon} >{weapon}</p>);
+    const members = house.swornMembers.map( (member, index) =>
+      <p key={index}>{member}</p> );
+
+    return (
+      <div
+        className="card"
+        onClick={() => this.toggleActiveMembers()}>
+        <h2>{house.name}</h2>
+        <p>{house.founded}</p>
+        <h3>House Seats</h3>
+        <h3>{seats}</h3>
+        <h3>House Titles</h3>
+        <div>{titles}</div>
+        <h3>Coat of Arms</h3>
+        <p>{house.coatOfArms}</p>
+        <h3>Ancestral Weapons</h3>
+        <div>{weapons}</div>
+        <h3>{house.words}</h3>
+        {this.state.showMembers &&
+        <div>{members}</div>
+        }
+      </div>
+    );
+  }
+}
 
 Card.propTypes = {
   house: PropTypes.object
