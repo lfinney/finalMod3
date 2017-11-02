e0d857b81071629a322ba2241307a97a03ed4bec
Luke Finney <lucaswfinney@gmail.com>
2017-11-02 12:13:41 -0600

    api call to retrieve member successful

--- src/utilities/databaseHelper.js
+++ src/utilities/databaseHelper.js
@@ -4,6 +4,33 @@ export const fetchHouseData = () => {
   return (dispatch) => {
     fetch(`http://localhost:3001/api/v1/houses`)
       .then(res => res.json())
+      .then(res => fetchSwornMembers(res))
       .then(res => dispatch(fetchData(res)));
   };
+};
+
+const fetchSwornMembers = (dataToParse) => {
+  const completeHouses = dataToParse.map( (house) => {
+    let incompleteHouse = {
+      name: house.name,
+      founded: house.founded,
+      seats: house.seats,
+      titles: house.titles,
+      coatOfArms: house.coatOfArms,
+      ancestralWeapons: house.ancestralWeapons,
+      words: house.words
+    };
+
+    house.swornMembers.map( member => {
+      fetch('http://localhost:3001/api/v1/character', {
+        method: 'POST',
+        body: JSON.stringify( {url: member} ),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(res => console.log(res));
+    });
+  });
 };
