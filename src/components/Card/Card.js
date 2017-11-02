e613a6dcc8bd6b1bf21ebf54ff9517cba94d187d
Luke Finney <lucaswfinney@gmail.com>
2017-11-02 13:14:59 -0600

    sworn members show when div clicked, but all houses members do

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ house }) => {
+const Card = ({ house, showActiveMembers, toggleActiveMembers }) => {
 
   const seats = house.seats.map( seat =>
     <p key={seat} >{seat}</p>);
@@ -9,9 +9,13 @@ const Card = ({ house }) => {
     <p key={title} >{title}</p>);
   const weapons = house.ancestralWeapons.map( weapon =>
     <p key={weapon} >{weapon}</p>);
+  const members = house.swornMembers.map( (member, index) =>
+    <p key={index}>{member}</p> )
 
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => toggleActiveMembers(true)}>
       <h2>{house.name}</h2>
       <p>{house.founded}</p>
       <h3>House Seats</h3>
@@ -23,6 +27,9 @@ const Card = ({ house }) => {
       <h3>Ancestral Weapons</h3>
       <div>{weapons}</div>
       <h3>{house.words}</h3>
+      {showActiveMembers &&
+      <div>{members}</div>
+      }
     </div>
   );
 };
--- src/components/CardDirectory/CardDirectory.js
+++ src/components/CardDirectory/CardDirectory.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Card from '../Card/Card';
 import PropTypes from 'prop-types';
 
-const CardDirectory = ({ housesInTheGame }) => {
+const CardDirectory = ({ housesInTheGame, showActiveMembers, toggleActiveMembers }) => {
   const houseInfo= housesInTheGame.map( (house, index) =>
     <Card
       house={house}
-      key ={`house - ${index}`}/>);
+      key ={`house - ${index}`}
+      showActiveMembers={showActiveMembers}
+      toggleActiveMembers={toggleActiveMembers}/>);
 
   return (
     <div className="card-container">{houseInfo}</div>
