8b6a63f115392c32786c0d40780146ff20f1166d
Luke Finney <lucaswfinney@gmail.com>
2017-11-02 10:17:46 -0600

    cards print given data for each house

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,9 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = ({ house }) => {
+  const seats = house.seats.map( seat => <p>{seat}</p>)
+  const titles = house.titles.map( title => <p>{title}</p>)
+  const weapons = house.ancestralWeapons.map( weapon => <p>{weapon}</p>)
   console.log(house);
   return (
-    <div>Winter is Here!</div>
+    <div>
+      <h2>{house.name}</h2>
+      <p>{house.founded}</p>
+      <h3>House Seats</h3>
+      <h3>{seats}</h3>
+      <h3>House Titles</h3>
+      <div>{titles}</div>
+      <h3>Coat of Arms</h3>
+      <p>{house.coatOfArms}</p>
+      <h3>Ancestral Weapons</h3>
+      <div>{weapons}</div>
+      <h3>{house.words}</h3>
+    </div>
   );
 };
 
