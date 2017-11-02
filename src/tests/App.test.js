13295b43be6bc3754dd0fe568409086cd6b0814b
Luke Finney <lucaswfinney@gmail.com>
2017-11-02 11:29:53 -0600

    install fetch-mock to fake api call

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import App from '../components/App/App';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+import housesMock from './mockData/housesMock';
 
-const middlewares = [];
+const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('App', () => {
+  const initialState = {
+    housesInTheGame: []
+  };
+  // const mockFunc = jest.fn();
+  const store = mockStore(initialState);
 
   it('should render component', () => {
-    const initialState = {
-      housesInTheGame: []
-    };
-    const mockFunc = jest.fn();
-    const store = mockStore(initialState);
-
     const wrapper = shallow(
       <App store={store} />);
 
+      console.log(wrapper.debug());
     expect(wrapper.length).toEqual(1);
   });
+
+  it('should make an api call when mounted', () => {
+    fetchMock.get(`http://localhost:3001/api/v1/houses`, {
+      status: 200,
+      body: housesMock
+    });
+    
+    const wrapper = mount(
+      <App store={store} />);
+      console.log(wrapper.debug());
+  })
 });
